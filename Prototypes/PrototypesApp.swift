d1bad6a7022d6f0a3133f6b994c7a7c462f84c75
Josh Nelson <jshnelson4@gmail.com>
2024-11-19 20:26:53 -0500

    Initial Commit

---
+++ Prototypes/PrototypesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypesApp.swift
+//  Prototypes
+//
+//  Created by Josh Nelson on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct PrototypesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
