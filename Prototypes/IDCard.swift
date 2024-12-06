37e3d6e1d0af96e1f0da95dc196df4478ec24b14
Josh Nelson <jshnelson4@gmail.com>
2024-12-05 22:37:28 -0500

    Init

---
+++ Prototypes/IDCard.swift
@@ -0,0 +1,18 @@
+//
+//  IDCard.swift
+//  Prototypes
+//
+//  Created by Josh Nelson on 11/19/24.
+//
+
+import SwiftUI
+
+struct IDCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IDCard()
+}
---
+++ Prototypes/MeshPractice.swift
@@ -0,0 +1,18 @@
+//
+//  MeshPractice.swift
+//  Prototypes
+//
+//  Created by Josh Nelson on 11/28/24.
+//
+
+import SwiftUI
+
+struct MeshPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeshPractice()
+}
