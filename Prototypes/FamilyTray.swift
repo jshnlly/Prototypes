e1f1915881192c07bdf165b83db24c0cbcf0e3b8
Josh Nelson <jshnelson4@gmail.com>
2024-12-16 21:52:38 -0500

    overlay

--- Prototypes/FamilyTray.swift
+++ Prototypes/FamilyTray.swift
@@ -25,139 +25,148 @@ struct FamilyTray: View {
     
     
     var body: some View {
-        VStack {
+        ZStack {
+            Rectangle()
+                .fill(Color.black)
+                .opacity(isExpanded ? 0.2 : 0)
+                .ignoresSafeArea()
+                .animation(FamilyTray.customAnimation, value: isExpanded)
             
-            Spacer()
-            
-            ZStack (alignment: .bottom) {
-                // Content Layer
-                VStack(alignment: .leading, spacing: 0) {
-                    HStack {
-                        Text("Dynamic tray")
-                            .font(.system(size: 24, weight: .semibold, design: .rounded))
-                            .foregroundStyle(Color.primary)
-                        
-                        Spacer()
-                        Image(systemName: "x.circle.fill")
-                            .font(.system(size: 24))
-                            .opacity(0.1)
-                            .onTapGesture {
-                                haptic.impactOccurred(intensity: 0.7)
-                                withAnimation(FamilyTray.customAnimation) {
-                                    
-                                    if isFullyExpanded {
+            VStack {
+                Spacer()
+                
+                ZStack (alignment: .bottom) {
+                    
+                    
+                    // Content Layer
+                    VStack(alignment: .leading, spacing: 0) {
+                        HStack {
+                            Text("Dynamic tray")
+                                .font(.system(size: 24, weight: .semibold, design: .rounded))
+                                .foregroundStyle(Color.primary)
+                            
+                            Spacer()
+                            Image(systemName: "x.circle.fill")
+                                .font(.system(size: 24))
+                                .opacity(0.1)
+                                .onTapGesture {
+                                    haptic.impactOccurred(intensity: 0.7)
+                                    withAnimation(FamilyTray.customAnimation) {
                                         
+                                        if isFullyExpanded {
+                                            
+                                            buttonWidth = UIScreen.main.bounds.width - 64
+                                            frameWidth = UIScreen.main.bounds.width - 32
+                                            frameShadow = 0.1
+                                            frameHeight = 280
+                                            frameRadius = 40
+                                            buttonOpacity = 1
+                                            buttonText = "One more time"
+                                            isFullyExpanded = false
+                                            
+                                        } else {
+                                            
+                                            isExpanded = false
+                                            buttonWidth = 200
+                                            frameWidth = 200
+                                            frameHeight = 56
+                                            frameRadius = 28
+                                            frameShadow = 0
+                                            buttonText = "Tap to expand"
+                                            buttonOpacity = 1
+                                        }
+                                    }
+                                }
+                        }
+                        .padding(.bottom, 16)
+                        
+                        ZStack {
+                            // First text block
+                            VStack {
+                                Text("This is a recreation of a tray system originally designed by Family, the Ethereum wallet. It's a great example of how SwiftUI can be used to create animations.")
+                                    .font(.system(size: 20, design: .rounded))
+                                    .foregroundStyle(Color.primary.opacity(0.5))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                            .opacity(isFullyExpanded ? 0 : 1)
+                            .scaleEffect(isExpanded ? 1 : 0.5)
+                            .offset(y: isExpanded ? -20 : 30)
+                            
+                            // Second text block (expanded state)
+                            VStack (spacing: 24) {
+                                Text("Step 2 jumps a bit higher than Step 1, we want to make the animation happen quickly")
+                                    .font(.system(size: 20, design: .rounded))
+                                    .foregroundStyle(Color.primary.opacity(0.5))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                
+                                Text("Tapping the X here will close this step, but not the entire sheet. To close the entire sheet, tap the X twice!")
+                                    .font(.system(size: 20, design: .rounded))
+                                    .foregroundStyle(Color.primary.opacity(0.5))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                            .opacity(isFullyExpanded ? 1 : 0)
+                            .scaleEffect(isFullyExpanded ? 1 : 0.5)
+                            .offset(y: isFullyExpanded ? 16 : 50)
+                        }
+                        .offset(y: isExpanded ? 0 : 100)
+                    }
+                    .padding(24)
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .opacity(isExpanded ? 1 : 0)
+                    
+                    // Button Layer
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.blue.opacity(buttonOpacity))
+                            .frame(width: buttonWidth, height: 56)
+                            .clipShape(RoundedRectangle(cornerRadius: 100))
+                        
+                        Text(buttonText)
+                            .font(.system(size: 20, weight: .semibold, design: .rounded))
+                            .foregroundStyle(.white)
+                    }
+                    .scaleEffect(isPressed ? 0.96 : 1.0)
+                    .animation(FamilyTray.customAnimation, value: isPressed)
+                    .gesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { _ in
+                                isPressed = true
+                            }
+                            .onEnded { _ in
+                                haptic.impactOccurred()
+                                isPressed = false
+                                
+                                withAnimation(FamilyTray.customAnimation) {
+                                    if !isExpanded {  // First expansion
+                                        isExpanded = true  // Move this up to animate together
                                         buttonWidth = UIScreen.main.bounds.width - 64
                                         frameWidth = UIScreen.main.bounds.width - 32
                                         frameShadow = 0.1
                                         frameHeight = 280
                                         frameRadius = 40
-                                        buttonOpacity = 1
                                         buttonText = "One more time"
-                                        isFullyExpanded = false
-                                        
-                                    } else {
-                                        
-                                        isExpanded = false
-                                        buttonWidth = 200
-                                        frameWidth = 200
-                                        frameHeight = 56
-                                        frameRadius = 28
-                                        frameShadow = 0
-                                        buttonText = "Tap to expand"
-                                        buttonOpacity = 1
+                                    } else {  // Second expansion
+                                        isFullyExpanded = true
+                                        frameHeight = 340
+                                        buttonOpacity = 0.3
+                                        buttonText = "Nice!"
                                     }
                                 }
                             }
-                    }
-                    .padding(.bottom, 16)
-                    
-                    ZStack {
-                        // First text block
-                        VStack {
-                            Text("This is a recreation of a tray system originally designed by Family, the Ethereum wallet. It's a great example of how SwiftUI can be used to create animations.")
-                                .font(.system(size: 20, design: .rounded))
-                                .foregroundStyle(Color.primary.opacity(0.5))
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        .opacity(isFullyExpanded ? 0 : 1)
-                        .scaleEffect(isExpanded ? 1 : 0.5)
-                        .offset(y: isExpanded ? -20 : 30)
-                        
-                        // Second text block (expanded state)
-                        VStack (spacing: 24) {
-                            Text("Step 2 jumps a bit higher than Step 1, we want to make the animation happen quickly")
-                                .font(.system(size: 20, design: .rounded))
-                                .foregroundStyle(Color.primary.opacity(0.5))
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            
-                            Text("Tapping the X here will close this step, but not the entire sheet. To close the entire sheet, tap the X twice!")
-                                .font(.system(size: 20, design: .rounded))
-                                .foregroundStyle(Color.primary.opacity(0.5))
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        .opacity(isFullyExpanded ? 1 : 0)
-                        .scaleEffect(isFullyExpanded ? 1 : 0.5)
-                        .offset(y: isFullyExpanded ? 16 : 50)
-                    }
-                    .offset(y: isExpanded ? 0 : 100)
+                    )
+                    .padding(.bottom, isExpanded ? 0 : 32)
                 }
-                .padding(24)
-                .frame(maxHeight: .infinity, alignment: .top)
-                .opacity(isExpanded ? 1 : 0)
-                
-                // Button Layer
-                ZStack {
-                    Rectangle()
-                        .fill(Color.blue.opacity(buttonOpacity))
-                        .frame(width: buttonWidth, height: 56)
-                        .clipShape(RoundedRectangle(cornerRadius: 100))
-                    
-                    Text(buttonText)
-                        .font(.system(size: 20, weight: .semibold, design: .rounded))
-                        .foregroundStyle(.white)
-                }
-                .scaleEffect(isPressed ? 0.96 : 1.0)
-                .animation(FamilyTray.customAnimation, value: isPressed)
-                .gesture(
-                    DragGesture(minimumDistance: 0)
-                        .onChanged { _ in
-                            isPressed = true
-                        }
-                        .onEnded { _ in
-                            haptic.impactOccurred()
-                            isPressed = false
-                            
-                            withAnimation(FamilyTray.customAnimation) {
-                                if !isExpanded {  // First expansion
-                                    isExpanded = true  // Move this up to animate together
-                                    buttonWidth = UIScreen.main.bounds.width - 64
-                                    frameWidth = UIScreen.main.bounds.width - 32
-                                    frameShadow = 0.1
-                                    frameHeight = 280
-                                    frameRadius = 40
-                                    buttonText = "One more time"
-                                } else {  // Second expansion
-                                    isFullyExpanded = true
-                                    frameHeight = 340
-                                    buttonOpacity = 0.3
-                                    buttonText = "Nice!"
-                                }
-                            }
-                        }
-                )
-                .padding(.bottom, isExpanded ? 0 : 32)
+                .frame(width: frameWidth, height: frameHeight)
+                .padding(.bottom, 24)
+                .background(isExpanded ? (colorScheme == .dark ? Color(UIColor.secondarySystemBackground) : Color(UIColor.systemBackground)) : Color.clear)
+                .clipShape(RoundedRectangle(cornerRadius: frameRadius))
+                .shadow(color: Color.secondary.opacity(0.15), radius: 12)
             }
-            .frame(width: frameWidth, height: frameHeight)
-            .padding(.bottom, 24)
-            .background(isExpanded ? (colorScheme == .dark ? Color(UIColor.secondarySystemBackground) : Color(UIColor.systemBackground)) : Color.clear)
-            .clipShape(RoundedRectangle(cornerRadius: frameRadius))
-            .shadow(color: Color.secondary.opacity(0.15), radius: 12)
-        }
-        .padding(.bottom, 32)
-        .edgesIgnoringSafeArea(.top)
-        .onAppear {
-            haptic.prepare()
+            .padding(.bottom, 32)
+            .edgesIgnoringSafeArea(.top)
+            .onAppear {
+                haptic.prepare()
+            }
         }
     }
 }
